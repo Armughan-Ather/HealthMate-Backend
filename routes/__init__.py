8056586f8e530cdde6eb7760cbe997903db0c842
Armughan-Ather <armughan.ather83@gmail.com>
2025-10-25 22:45:57 +0500

    bp schedules with issues

--- routes/__init__.py
+++ routes/__init__.py
@@ -13,8 +13,8 @@ from routes.medication_schedules import router as medication_schedules_router
 # from routes.adhoc_medication_logs import router as adhoc_medication_logs_router
 
 # from routes.chats import router as chats_router
-# from routes.messages import router as messages_router
-# from routes.bp_schedules import router as bp_schedules_router
+from routes.messages import router as messages_router
+from routes.bp_schedules import router as bp_schedules_router
 # from routes.sugar_schedules import router as sugar_schedules_router
 # from routes.scheduled_bp_logs import router as bp_logs_router
 # from routes.scheduled_sugar_logs import router as sugar_logs_router
@@ -39,7 +39,7 @@ router.include_router(medication_schedules_router, prefix="/medication_schedules
 
 # router.include_router(chats_router, prefix="/chats", tags=["Chats"])
 # router.include_router(messages_router, prefix="/messages", tags=["Messages"])
-# router.include_router(bp_schedules_router, prefix="/bp_schedules", tags=["BP_Schedules"])
+router.include_router(bp_schedules_router, prefix="/bp_schedules", tags=["BP_Schedules"])
 # router.include_router(bp_logs_router, prefix="/bp_logs", tags=["BP_Logs"])
 # router.include_router(sugar_schedules_router, prefix="/sugar_schedules", tags=["Sugar_Schedules"])
 # router.include_router(sugar_logs_router, prefix="/sugar_logs", tags=["Sugar_Logs"])
